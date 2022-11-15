f47a6fcbcaac28b44816017665e8c309c4b53440
lksrinivaskarthik <srinivaskarthik33@gmail.com>
2022-11-15 22:49:23 +0530

    Get Data from MongoDB

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const router = express.Router();
 const mongoose = require('mongoose')
+const Video = require('../models/video') 
 
-const db="https://cloud.mongodb.com/v2/6373aa4e13ca3c1f7de74880#metrics/replicaSet/6373ab5459c198791cc94739/explorer/videoplayer"
+const db = "mongodb+srv://karthik:karTHIK1234@cluster0.fxinup0.mongodb.net/videoplayer?retryWrites=true&w=majority"
 mongoose.Promise=global.Promise
+
 mongoose.connect(db,function(err){
     if(err){
-        console.error("Error! "+err)
+        console.error("Error!"+err);
     }
-});
-router.get('/',(req,res)=>{
-    res.send('ap1 works');
+})
+
+
+router.get('/videos',(req,res)=>{
+    console.log("Get Request for all Videos")
+    Video.find({})
+    .exec(function(err,videos){
+        if(err){
+            console.log("Error in retreiving videos")
+        }
+        else{
+            res.json(videos)
+        }
+    })
 });
 
 
