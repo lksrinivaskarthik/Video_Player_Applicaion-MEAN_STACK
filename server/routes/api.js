a51eaf50a3f7fc90bb4b77366b8956f504a2cbb5
lksrinivaskarthik <srinivaskarthik33@gmail.com>
2022-11-16 18:30:45 +0530

    Posting, Updating and Deleting data from MongoDB

--- server/routes/api.js
+++ server/routes/api.js
@@ -21,10 +21,73 @@ router.get('/videos',(req,res)=>{
             console.log("Error in retreiving videos")
         }
         else{
-            res.json(videos)
+            res.send(videos)
         }
     })
 });
+
+router.get('/videos/:id',(req,res)=>{
+    console.log("Get request to get particular video")
+    Video.findById(req.params.id)
+    .exec(function(err,video){
+        if(err){
+            console.log("Error in fetching the video");
+        }else{
+            res.send(video);
+        }
+    });
+});
+
+router.post('/video',(req,res)=>{
+    console.log("post video")
+    var newVideo = new Video;
+    newVideo.title=req.body.title
+    newVideo.url = req.body.url;
+    newVideo.description=req.body.description
+    newVideo.save(function(err,insertedVideo){
+        if(err){
+            console.log('Error saving video');
+        }else{
+            res.send(insertedVideo)
+        }
+    })
+
+});
+
+//Put request uses findByIdAndUpdate function which takes 4 arguments (id, new_values to be set, if new is true then it returns the updated Video with new values set,function to handle error)
+
+router.put('/videos/:id',(req,res)=>{
+    console.log("Update the defined id value");
+    Video.findByIdAndUpdate(req.params.id,{
+        $set: {title:req.body.title, url:req.body.url, description:req.body.description}
+    },
+    {
+        new:true
+    },
+    function(err,updatedVideo){
+        if(err){
+            console.log("Error in updating the video")
+        }else{
+            res.send(updatedVideo)
+        }
+    }
+    )
+});
+
+router.delete('/videos/:id',(req,res)=>{
+    console.log("Delete the specified video");
+    Video.findByIdAndRemove(req.params.id,function(err,deletedVideo){
+        if(err){
+            console.log("Error in deleting the video")
+        }else{
+            res.send(deletedVideo)
+        }
+    })
+})
+
+
+
+
 
 
 
