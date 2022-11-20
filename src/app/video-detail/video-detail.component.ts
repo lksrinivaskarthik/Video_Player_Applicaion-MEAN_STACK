fb8138c22abf18e9dd5fd40a5ecfc907d6a26796
lksrinivaskarthik <srinivaskarthik33@gmail.com>
2022-11-20 11:41:57 +0530

    Designing Video Detail Component UI

--- src/app/video-detail/video-detail.component.ts
+++ src/app/video-detail/video-detail.component.ts
@@ -9,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class VideoDetailComponent implements OnInit {
 
   video:any;
+  public editTitle:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  //if we change from one title to another title we need to change back to its original state so in order to do that we use
+  //  ngOnChanges lifecycle hook
+
+  ngOnChanges(){
+    this.editTitle=false;
+  }
+
+  onTitleClick(){
+    this.editTitle=true;
   }
 
 }
